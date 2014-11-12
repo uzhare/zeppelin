38dadf36cc07258bd0dcafe004464f8be0ca584f
Uzhare Farooq <uzharefarooq@gmail.com>
2014-11-13 00:46:08 +0530

    Updated views for displaying the data in simple way

--- finance/views.py
+++ finance/views.py
@@ -28,16 +28,10 @@ class DisplayView(TemplateView):
         ticker_object = Ticker.objects.get(id=ticker)
         url = "http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,%s&f=nsl1op&e=.csv" % ticker_object.ticker
         data = requests.get(url, stream=True)
-        
-        with open('help.csv', 'wb') as f:
-            for chunk in data.iter_content(1024):
-                f.write(chunk)
-        import csv
-        with open('help.csv', 'rb') as f:
-            read = csv.reader(f, delimiter=',', quotechar='"')
-            for row in read:
-                result = row
-        print result
+        data_list = []
+        for chunk in data.iter_content(1024):
+            data_list.append(chunk)
+        result = data_list[0].split(',')
         kwargs.update({'result': result,})
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
